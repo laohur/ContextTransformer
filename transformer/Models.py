3590652de90b9fb6e3a19448a6e1e7dc23330165
laohur <laohur@gmail.com>
2019-06-11 23:31:46 +0800

    71.1层，src+ctx,tgt-ctx。110.txt 0-8,5-17,11-24 v25 很多“可以、没有”。

--- transformer/Models.py
+++ transformer/Models.py
@@ -225,11 +225,11 @@ class ContextTransformer(nn.Module):
     def forward(self, src_seq, src_pos, ctx_seq, ctx_pos, tgt_seq, tgt_pos):
         tgt_seq, tgt_pos = tgt_seq[:, :-1], tgt_pos[:, :-1]
 
-        ctx_output = None
+        ctx_output, *_ = self.ctx_encoder(ctx_seq, ctx_pos)  # batch*ctx_seq*512
         # encoder = self.encoder if random.random() < 0.01 else None
         src_output, *_ = self.src_encoder(src_seq, src_pos, ctx_seq, ctx_output, encoder=self.encoder)  # batch*src_seq*512
 
-        ctx_output, *_ = self.ctx_encoder(ctx_seq, ctx_pos)  # batch*ctx_seq*512
+        ctx_output = None
         # encoder = self.encoder if random.random() < 0.01 else None
         tgt_output, *_ = self.tgt_decoder(tgt_seq, tgt_pos, ctx_seq, ctx_output, src_seq, src_output, encoder=self.encoder)
 
